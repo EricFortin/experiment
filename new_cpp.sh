e6da12d9abefad95fb0737ca2a36d0183e25203e
Eric Fortin <eric.fortin2@gmail.com>
2014-01-21 21:22:24 -0500

    Added boost support.

--- new_cpp.sh
+++ new_cpp.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+# Load config for different machine setup.
+source cpp/.experiment_config
+
+# Define default options.
+NO_BOOST=false
+
+
+usage() {
+    echo "usage: new_cpp [options] project_name"
+    exit 0
+}
+
+
+# Parse options.
 while test $# -gt 0; do
     case "$1" in
         -h|--help)
-            echo "usage: new_cpp [options] project_name"
-            exit 0
+            usage
+            ;;
+        --no-boost)
+            NO_BOOST=true
+            shift
+            ;;
+        --boost-version)
+            shift
+            BOOST_VERSION="$1"
+            shift
             ;;
         *)
             PROJECT_NAME=$1
@@ -13,7 +35,12 @@ while test $# -gt 0; do
     esac
 done
 
+if [[ -z "$PROJECT_NAME" ]] ; then usage
+fi
+
+# Create project, generate makefiles and build.
 echo "Creating project:" $PROJECT_NAME
+
 
 pushd ./cpp
 mkdir $PROJECT_NAME
@@ -21,8 +48,19 @@ cp template/* $PROJECT_NAME
 
 pushd $PROJECT_NAME
 
-cmake .
+if $NO_BOOST ; then
+    echo "Excluding boost library."
+    CMAKE_ARGUMENTS="$CMAKE_ARGUMENTS -DNO_BOOST=ON"
+else
+    echo "Using Boost version: $BOOST_VERSION"
+    CMAKE_ARGUMENTS="$CMAKE_ARGUMENTS -DBOOST_PATH=$BOOST_PATH/$BOOST_VERSION/"
+fi
+
+echo "CMAKE_ARGUMENTS: $CMAKE_ARGUMENTS"
+
+cmake $CMAKE_ARGUMENTS .
 make
 ./experiment
 
 gvim main.cpp &
+
